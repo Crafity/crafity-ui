407dc681ccdee8df1a3936bd50d71124f850f9c1
Bart Riemens <briemens@crafity.com>
2014-01-08 13:31:40 +0100

    Added a new menu control which can hold multiple menu panels

--- js/crafity.html.Menu.js
+++ js/crafity.html.Menu.js
@@ -5,7 +5,42 @@
 
 	(function (html) {
 
+		function Menu() {
+			this.addClass("main menu");
+		}
+
+		Menu.prototype = new crafity.html.Element("div");
+		Menu.prototype.addMenuPanel = function (menuPanel) {
+			var self = this;
+			this.append(menuPanel);
+			menuPanel.on("selected", function (name, menuPanel, menuItem) {
+				self.getChildren().forEach(function (mp) {
+					if (mp !== menuPanel) {
+						mp.deselectAll();
+					}
+				});
+				self.emit("selected " + name, menuPanel, menuItem);
+				self.emit("selected", name, menuPanel, menuItem);
+			});
+			return self;
+		};
+		Menu.prototype.addMenuPanels = function (menuPanels) {
+			var self = this;
+			if (menuPanels instanceof Array) {
+				menuPanels.forEach(function (menuPanel) {
+					self.addMenuPanel(menuPanel);
+				});
+			}
+			if (menuPanels instanceof html.Element) {
+				self.addMenuPanel(menuPanel);
+			}
+			return self;
+		};
+
+		html.Menu = Menu;
+
 		function MenuPanel(name, init) {
+			this.name = name;
 			this._menuItems = new html.Element("ul");
 			this.addClass("menu-panel hidden");
 			if (name) {
@@ -19,16 +54,23 @@
 		}
 
 		MenuPanel.prototype = new html.Element("div");
+		MenuPanel.prototype.deselectAll = function () {
+			this._menuItems.getChildren().forEach(function (mi) {
+				mi.removeClass("selected");
+			});
+		};
 		MenuPanel.prototype.addMenuItem = function (menuItem) {
 			var self = this;
 			this._menuItems.append(menuItem);
-			menuItem.on("click", function (clickedMenuItem) {
+			menuItem.on("click", function () {
 				self._menuItems.getChildren().forEach(function (mi) {
-					if (mi !== clickedMenuItem) {
+					if (mi !== menuItem) {
 						mi.removeClass("selected");
 					}
 				});
-				clickedMenuItem.addClass("selected");
+				menuItem.addClass("selected");
+				self.emit("selected " + self.name + " " + menuItem.name, self, menuItem);
+				self.emit("selected", self.name + " " + menuItem.name, self, menuItem);
 			});
 			return self;
 		};
@@ -49,7 +91,7 @@
 		function MenuItem(name, callback) {
 			var self = this;
 
-			self.on("click", callback);
+			callback && self.on("click", callback);
 
 			var anchor = new html.Element("a");
 			anchor.attr("href", "#" + name);
@@ -61,15 +103,19 @@
 			});
 			anchor.text(name);
 			this.addClass("menuitem");
-
+			this.name = name;
 			this.append(anchor);
 		}
 
 		MenuItem.prototype = new html.Element("li");
 		MenuItem.prototype.select = function () {
-			this.addClass("selected");
-			this.emit("selectionChanged", this);
-			this.emit("selected", this);
+			var self = this;
+			self.addClass("selected");
+			setTimeout(function () {
+				self.emit("selectionChanged", self);
+				self.emit("selected", self);
+				self.emit("click", self);
+			}, 0);
 			return this;
 		};
 		MenuItem.prototype.selected = function () {
@@ -82,7 +128,7 @@
 			return this;
 		};
 		html.MenuItem = MenuItem;
-		
+
 	}(crafity.html = crafity.html || {}));
 
 }(window.crafity = window.crafity || {}));
