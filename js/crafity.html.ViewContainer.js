a0cca87463c931793360c5e84b43f85623b38525
Bart Riemens <briemens@crafity.com>
2014-01-08 13:30:44 +0100

    Introduced a view container that shows a single view (or element) at the time

---
+++ js/crafity.html.ViewContainer.js
@@ -0,0 +1,33 @@
+/*jslint browser: true, nomen: true, vars: true, white: true*/
+
+(function (crafity) {
+	"use strict";
+
+	(function (html) {
+
+		function ViewContainer() {
+			var self = this;
+			this.activate = function (view) {
+				self.getChildren().forEach(function (child) {
+					if (child === view) {
+						child.show();
+					} else {
+						child.hide();
+					}
+				});
+				self.append(view);
+			};
+			this.deactivateAll = function () {
+				self.getChildren().forEach(function (child) {
+					child.hide();
+				});
+			};
+		}
+		ViewContainer.prototype = new crafity.html.Element("div");
+		
+		html.ViewContainer = ViewContainer;
+		
+	}(crafity.html = crafity.html || {}));
+
+}(window.crafity = window.crafity || {}));
+		
