f06e564c741c2dca5fdfb61021651e1ef4f2593c
Galina Slavova <galina@crafity.com>
2014-01-09 17:35:59 +0100

    Added windows support to the shortcut key logic (cmd is ctrl on windows)

--- js/crafity.keyboard.js
+++ js/crafity.keyboard.js
@@ -6,38 +6,44 @@
 
 		var emitter = new crafity.core.EventEmitter();
 
+		function cmdOrCrl(e) {
+			// (navigator.platform.match(/Mac/) ? e.metaKey : e.ctrlKey
+			return ((e.metaKey && !e.ctrlKey) || (!e.metaKey && e.ctrlKey));
+		}
+	
 		window.addEventListener("keydown", function (e) {
-			if (e.shiftKey && (e.metaKey || e.keyIdentifier === "U+004D") && e.which === 77) {
+			
+			if (e.shiftKey && cmdOrCrl(e) && e.which === 77) {
 				console.log("cmd+shft+m");
 				emitter.emit("cmd+shft+m", e);
 				e.preventDefault();
 				return false;
 			}
-			if (!e.shiftKey && e.metaKey && e.which === 69) {
+			if (!e.shiftKey && cmdOrCrl(e) && e.which === 69) {
 				console.log("cmd+e");
 				emitter.emit("cmd+e", e);
 				e.preventDefault();
 				return false;
 			}
-			if (!e.shiftKey && e.metaKey && e.which === 76) {
+			if (!e.shiftKey && cmdOrCrl(e) && e.which === 76) {
 				console.log("cmd+l");
 				emitter.emit("cmd+l", e);
 				e.preventDefault();
 				return false;
 			}
 			// U+0046 70 
-			if (!e.shiftKey && e.metaKey && e.which === 70) {
+			if (!e.shiftKey && cmdOrCrl(e) && e.which === 70) {
 				console.log("cmd+f");
 				emitter.emit("cmd+f", e);
 				e.preventDefault();
 				return false;
 			}
-			if (!e.shiftKey && e.metaKey && e.which === 78) {
+			if (!e.shiftKey && cmdOrCrl(e) && e.which === 78) {
 				emitter.emit("cmd+n", e);
 				e.preventDefault();
 				return false;
 			}
-			if (!e.shiftKey && e.metaKey && e.altKey && e.which === 192) {
+			if (!e.shiftKey && cmdOrCrl(e) && e.altKey && e.which === 192) {
 				emitter.emit("cmd+opt+n", e);
 				e.preventDefault();
 				return false;
@@ -48,7 +54,8 @@
 				e.preventDefault();
 				return false;
 			}
-			if (e.which === 83 && (navigator.platform.match(/Mac/) ? e.metaKey : e.ctrlKey)) {
+			
+			if (e.which === 83 && cmdOrCrl(e)) {
 				console.log("cmd+s");
 				emitter.emit("cmd+s", e);
 				e.preventDefault();
