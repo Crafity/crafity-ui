8e4fc5bfc2dd3b520b1fd81d130722b312258e8b
Bart Riemens <briemens@crafity.com>
2014-01-16 20:11:52 +0100

    Cleaned up a little bit

--- js/crafity.html.Element.js
+++ js/crafity.html.Element.js
@@ -57,19 +57,11 @@
 			this._data = data;
 			this.emit("dataChanged", data);
 			return this;
-		};		
+		};
 		Element.prototype.element = function () {
 			if (!this._element) {
 				this._element = document.createElement(this.getType());
-				//this._element.self = this;
-			} 
-//			else if (this._element.self !== this) {
-//				var _element = this._element; //.cloneNode();
-//				delete this._element;
-//				this._element = _element;
-//				this._element.self = this;
-//				console.log("Cloning", this._element.self, this);
-//			}
+			}
 			return this._element;
 		};
 		Element.prototype.prepend = function (children) {
