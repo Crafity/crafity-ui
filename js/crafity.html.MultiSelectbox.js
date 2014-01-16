021407708df5d03e5b7d116cc1ff4847bb9116b4
Bart Riemens <briemens@crafity.com>
2014-01-16 20:13:17 +0100

    Adding a new control called MultiSelectbox

---
+++ js/crafity.html.MultiSelectbox.js
@@ -0,0 +1,17 @@
+/*jslint browser: true, nomen: true, vars: true, white: true*/
+
+(function (crafity) {
+	"use strict";
+
+	(function (html) {
+		var EMPTY_SELECT_VALUE = " ";
+		
+		function MultiSelectbox() {
+			var self = this;
+		}
+		
+		html.MultiSelectbox = MultiSelectbox;
+
+	}(crafity.html = crafity.html || {}));
+
+}(window.crafity = window.crafity || {}));
