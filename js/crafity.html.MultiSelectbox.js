2cf4284294063baa7552617c69b473456d91e7fb
Bart Riemens <briemens@crafity.com>
2014-01-29 13:07:49 +0100

    Improved the multiselectbox

--- js/crafity.html.MultiSelectbox.js
+++ js/crafity.html.MultiSelectbox.js
@@ -8,8 +8,41 @@
 		
 		function MultiSelectbox() {
 			var self = this;
+
+			this._selectedValue = new html.Element("span").text(EMPTY_SELECT_VALUE);
+			this.append(this._selectedValue);
+
+			this._optionList = new html.Element("div");
+			this.append(this._optionList);
+			
+			this._options = [];
+			this.addClass("multi selectbox edit");
+			this.tabindex("0");
+
+			this.on("readonlyChanged", function (bool) {
+				if (bool === true) {
+					self.removeClass("edit").addClass("readonly").tabindex("-1");
+				}
+				if (bool === false) {
+					self.addClass("edit").removeClass("readonly").tabindex(null);
+				}
+			});
 		}
 		
+		MultiSelectbox.prototype = new html.Element("div");
+		MultiSelectbox.prototype.options = function (options) {
+			var self = this;
+			if (options === undefined) {
+				//return self._optionList.options();
+				return self;
+			}
+			Object.keys(options).forEach(function (key) {
+				self.append(new html.Element("div").text(options[key]));
+			});
+			//self._optionList.options(options);
+			return self;
+
+		};
 		html.MultiSelectbox = MultiSelectbox;
 
 	}(crafity.html = crafity.html || {}));
